fbfcac209db47d479336145bfbfa4e26a9cc67dd
clement <clement.debize.se2@gmail.com>
2017-11-29 17:36:31 +0100

    [robust_estimation] added a generic kernel for all LORANSAC problems (by Simone)

--- src/aliceVision/robustEstimation/LORansacKernelAdaptor.hpp
+++ src/aliceVision/robustEstimation/LORansacKernelAdaptor.hpp
@@ -14,6 +14,98 @@ namespace robustEstimation {
 
 
 /**
+ * @brief A generic kernel used for the LORANSAC framework.
+ *
+ * @tparam minSample The minimum number of samples that allows to solve the problem
+ * @tparam minSampleLS The minimum number of samples that allows to solve the problem in a least squared manner.
+ * @tparam Model The class representing the model to estimate.
+ */
+template <int minSample,
+          int minSampleLS,
+          typename Model>
+class LORansacGenericKernel
+{
+  public:
+  
+  enum
+  {
+    MINIMUM_SAMPLES = minSample,
+    MINIMUM_LSSAMPLES = minSampleLS
+  };
+
+  /**
+   * @brief The constructor;
+   */
+  LORansacGenericKernel() = default;
+
+  /**
+   * @brief This function is called to estimate the model from the minimum number
+   * of sample \p minSample (i.e. minimal problem solver).
+   * @param[in] samples A vector containing the indices of the data to be used for
+   * the minimal estimation.
+   * @param[out] models The model(s) estimated by the minimal solver.
+   */
+  virtual void Fit(const std::vector<std::size_t> &samples, std::vector<Model> *models) const = 0;
+
+  /**
+   * @brief This function is called to estimate the model using a least squared
+   * algorithm from a minumum of \p minSampleLS.
+   * @param[in] inliers An array containing the indices of the data to use.
+   * @param[out] models The model(s) estimated using the least squared algorithm.
+   * @param[in] weights An optional array of weights, one for each sample
+   */
+  virtual void FitLS(const std::vector<std::size_t> &inliers, 
+                      std::vector<Model> *models, 
+                      const std::vector<double> *weights = nullptr) const = 0; 
+
+  /**
+   * @brief Function used to estimate the weights, typically used by the least square algorithm.
+   * @param[in] model The model against which the weights are computed.
+   * @param[in] inliers The array of the indices of the data to be used.
+   * @param[out] vec_weights The array of weight of the same size as \p inliers.
+   * @param[in] eps An optional threshold to max out the value of the threshold (typically
+   * to avoid division by zero or too small numbers).
+   */
+  virtual void computeWeights(const Model & model, 
+                              const std::vector<std::size_t> &inliers, 
+                              std::vector<double> & vec_weights, 
+                              const double eps = 0.001) const = 0; 
+
+  /**
+   * @brief Function that computes the estimation error for a given model and a given element.
+   * @param[in] sample The index of the element for which the error is computed.
+   * @param[in] model The model to consider.
+   * @return The estimation error for the given element and the given model.
+   */
+  virtual double Error(std::size_t sample, const Model &model) const = 0;
+
+  /**
+   * @brief Function that computes the estimation error for a given model and all the elements.
+   * @param[in] model The model to consider.
+   * @param[out] vec_errors The vector containing all the estimation errors for every element.
+   */
+  virtual void Errors(const Model & model, std::vector<double> & vec_errors) const = 0;
+
+  /**
+   * @brief Function used to unnormalize the model.
+   * @param[in,out] model The model to unnormalize.
+   */
+  virtual  void Unnormalize(Model * model) const = 0;
+
+  /**
+   * @brief The number of elements in the data.
+   * @return the number of elements in the data.
+   */
+  virtual std::size_t NumSamples() const = 0; 
+
+  /**
+   * @brief The destructor.
+   */
+  virtual ~LORansacGenericKernel( ) = default;
+
+};
+
+/**
  * @brief The generic kernel to be used for LORansac framework.
  * 
  * @tparam SolverArg The minimal solver able to find a solution from a
